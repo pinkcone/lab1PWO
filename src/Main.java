6c7b6f4d27f35bb1d7ba8ec1d048820d99899878
pinkcone <szymondudek4@gmail.com>
2024-10-10 13:55:07 +0200

    fix/repair
    feat/add_hello
    
    fix/fixed_duplicated_lines

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        for (int i = 0; i < 50; i++) {
+            System.out.println("Hello world!");
+        }
+
     }
 }
